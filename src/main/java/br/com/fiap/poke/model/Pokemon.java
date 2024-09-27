3506c6913ccb105d607297a2d2049764ae2ab3b0
LuizHNR <luizhneri12@gmail.com>
2024-09-27 08:51:42 -0300

    Formas

--- src/main/java/br/com/fiap/poke/model/Pokemon.java
+++ src/main/java/br/com/fiap/poke/model/Pokemon.java
@@ -2,20 +2,22 @@
 
 package br.com.fiap.poke.model;
 
-import java.util.List;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class Pokemon {
 
     private int id;
     private String name;
+    private ArrayList<Types> types;
 
     @Override
     public String toString(){
         return "\n------------Pokemon-------------\n" +
                 "Numero da pokedex: " + id +
-                "\nNome do Pokemon: " + name;
+                "\nNome do Pokemon: " + name +
+                "\nTipo do Pokemon: " + types;
     }
-
 
     public int getId() {
         return id;
@@ -33,4 +35,11 @@ public class Pokemon {
         this.name = nome;
     }
 
+    public ArrayList<Types> getTypes() {
+        return types;
+    }
+
+    public void setTypes(ArrayList<Types> types) {
+        this.types = types;
+    }
 }
